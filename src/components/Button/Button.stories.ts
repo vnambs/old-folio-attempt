311ff1b56e37e5abf0464bf23822d6a9996dff96
Voary Nambinina <vnambs@gmail.com>
2024-04-16 18:22:34 +0300

    feat(button): adding my button
    creat button and button stories :tada:

---
+++ src/components/Button/Button.stories.ts
@@ -0,0 +1,83 @@
+import { Meta, StoryObj } from "@storybook/react";
+import Button, { ButtonProps } from "./Button";
+
+const meta: Meta<ButtonProps> = {
+  title: "Components/Button",
+  component: Button,
+  argTypes: {
+    children: {
+      type: "string",
+    },
+    disabled: {
+      type: "boolean",
+    },
+    className: {
+      type: "string",
+    },
+  },
+};
+
+const violet = "theme-violet";
+
+export default meta;
+
+export const Primary: StoryObj<ButtonProps> = {
+  args: {
+    children: "Button",
+  },
+};
+export const Disabled: StoryObj<ButtonProps> = {
+  args: {
+    children: "Button",
+    disabled: true,
+  },
+};
+export const Secondary: StoryObj<ButtonProps> = {
+  args: {
+    children: "Button",
+    variant: "secondary",
+  },
+};
+export const SecondaryDisabled: StoryObj<ButtonProps> = {
+  args: {
+    children: "Button",
+    variant: "secondary",
+    disabled: true,
+  },
+};
+
+export const Tertiary: StoryObj<ButtonProps> = {
+  args: {
+    children: "Button",
+    variant: "tertiary",
+  },
+};
+export const TertiaryDisabled: StoryObj<ButtonProps> = {
+  args: {
+    children: "Button",
+    variant: "tertiary",
+    disabled: true,
+  },
+};
+
+export const Violet: StoryObj<ButtonProps> = {
+  args: {
+    children: "Button",
+    className: violet,
+  },
+};
+
+export const VioletSecondary: StoryObj<ButtonProps> = {
+  args: {
+    children: "Button",
+    variant: "secondary",
+    className: violet,
+  },
+};
+export const VioletTertiary: StoryObj<ButtonProps> = {
+  args: {
+    children: "Button",
+    className: violet,
+    variant: "tertiary",
+  },
+};
